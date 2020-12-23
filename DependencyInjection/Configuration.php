e8c8810b34ca68b7ea27d62195b76c4bdd876705
Mikhail Shtukin <shtumi@tut.by>
2020-12-23 19:13:16 +0200

    Symfony 5 support

--- DependencyInjection/Configuration.php
+++ DependencyInjection/Configuration.php
@@ -42,8 +42,13 @@ class Configuration implements ConfigurationInterface
      */
     public function getConfigTreeBuilder()
     {
-        $treeBuilder = new TreeBuilder();
-        $rootNode = $treeBuilder->root('slot_mandrill');
+        $treeBuilder = new TreeBuilder('slot_mandrill');
+        if (method_exists($treeBuilder, 'getRootNode')) {
+            $rootNode = $treeBuilder->getRootNode();
+        } else {
+            // BC layer for symfony/config 4.1 and older
+            $rootNode = $treeBuilder->root('snc_redis');
+        }
 
         $rootNode
         ->children()
